2e796f2960448373b98578a51a542caa095a21c1
Erfan Davoodi Nasr <davoodinasr.erfan@gmail.com>
2026-01-05 21:11:48 +0330

    init commit

---
+++ src/main/java/com/ernoxin/zarinpaljavasdk/ZarinPalJavaSdkApplication.java
@@ -0,0 +1,13 @@
+package com.ernoxin.zarinpaljavasdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarinPalJavaSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZarinPalJavaSdkApplication.class, args);
+    }
+
+}
